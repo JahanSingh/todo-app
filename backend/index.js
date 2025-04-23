3044867d34698d5239008bb599fae56769382080
JahanSingh <jahansingh113@gmail.com>
2025-04-23 12:54:15 +0530

    backemd reviewed/error-fixed amd tested successfully

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const { todo } = require("./db");
 const app = express()
 
 app.use(express.json())
-app.post("/todo", function async (req, res) {
+app.post("/todo", async function (req, res) {
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
     if (!parsedPayload.success) {
@@ -15,7 +15,7 @@ app.post("/todo", function async (req, res) {
     }
     await todo.create({
         title: createPayload.title,
-        description: createPayload.description,
+        description: createPayload.desc,
         completed: false
     })
     res.json({
@@ -28,7 +28,7 @@ app.get("/todos", async function (req, res) {
         todos
     })
 })
-app.put("/completed", async     function (req, res) {
+app.put("/completed", async function (req, res) {
     const updatePayload = req.body;
     const parsedPayload = updateTodo.safeParse(updatePayload);
     if (!parsedPayload.success) {
@@ -37,14 +37,14 @@ app.put("/completed", async     function (req, res) {
         })
         return;
     }
-    await todo.update({
+    await todo.updateOne({
         _id: req.body.id
-    },  {
-            completed: true
+    }, {
+        completed: true
     })
     res.json({
         msg: "todo marked as completed"
     })
-)
+})
 
 app.listen(3000);
